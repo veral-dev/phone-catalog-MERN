f34f5c230a2ff31c0b5761bbfb1039210ba901c9
david <veral.david@gmail.com>
2021-11-25 16:29:05 +0100

    connect client with heroku

--- client/src/services/config-axios.js
+++ client/src/services/config-axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const clientAxios = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  // baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://phone-catalog-srv.herokuapp.com/api'
 });
 
 export default clientAxios;
